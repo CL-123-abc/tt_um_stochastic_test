6fa81823295527790b3ec63d6735e85d6012cbb0
CL123abc <lciecen@gmail.com>
2024-08-04 18:42:12 -0700

    Update project.v

--- src/project.v
+++ src/project.v
@@ -42,9 +42,11 @@ module tt_um_stochastic_test_CL123abc(
     end else begin
         // Increment counter on each clock cycle
         lfsr_1[0] <= lfsr_1[27] ^ lfsr_1[30] ;
+		#1;
         lfsr_1[30:1] <=lfsr_1[29:0] ;
 
 	    lfsr_2[0] <= lfsr_2[12] ^ lfsr_2[16] ; //using different gates to get different pattern
+		#1;
         lfsr_2[30:1] <=lfsr_2[29:0] ;
 
 	    // Comparator used to generate Bipolar Stochastic Number from 4-bit probability.
