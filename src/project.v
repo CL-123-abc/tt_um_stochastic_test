09e279215afc2b5390c15698b9a55032c36b7907
CL123abc <lciecen@gmail.com>
2024-08-10 18:44:11 -0700

    Update project.v

--- src/project.v
+++ src/project.v
@@ -72,8 +72,8 @@ module tt_um_stochastic_test_CL123abc(
 end  
   // All output pins must be assigned. If not used, assign to 0.
   assign uo_out[0] = 0;
-  assign uo_out[3:1] = output_prob;
-  assign uo_out[4] = overflow;
+  assign uo_out[3:1] = clk? output_prob:uo_out[3:1];
+  assign uo_out[4] = clk? overflow:uo_out[4];
   assign uio_out = 0;
   assign uio_oe  = 0;
   assign uo_out[7:5] = 3'b000;
