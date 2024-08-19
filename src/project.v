16fd97eb4279d425b63f0d6b6b46a828d7df46c8
CL123abc <lciecen@gmail.com>
2024-08-19 08:28:45 -0700

    Update project.v

--- src/project.v
+++ src/project.v
@@ -24,11 +24,10 @@ module tt_um_stochastic_test_CL123abc(
 );
 	reg [30:0] lfsr_1, lfsr_2;
     reg SN_Bit_1, SN_Bit_2, SN_Bit_Out; 
-    reg [3:0] clk_counter;
-	reg [2:0] prob_counter;
-	reg [2:0] output_prob;
+	reg [7:0] clk_counter;
+	reg [6:0] prob_counter;
     reg over_flag;
-	reg overflow;
+	reg [31:0] average;
     
     always @(posedge clk or posedge rst_n) begin
         if (rst_n) begin
@@ -39,9 +38,7 @@ module tt_um_stochastic_test_CL123abc(
         SN_Bit_Out <= 1'b0; 
 	    clk_counter <= 4'b0000; // Reset clk counter
 	    prob_counter <= 3'b000; // Reset output counter
-		output_prob <= 3'b000;
 	    over_flag <= 0; // Reset overflag
-	    overflow <= 0;
         end else begin
         // Increment counter on each clock cycle
         lfsr_1[0] <= lfsr_1[27] ^ lfsr_1[30] ;
@@ -60,32 +57,28 @@ module tt_um_stochastic_test_CL123abc(
 	    
 	    // To convert back to binary probability, use an up-counter, outputting the number of 1s in every 8 bits
 	    if (SN_Bit_Out == 1) begin
-	        if (prob_counter == 3'b111) begin
+			if (prob_counter == 7'b1111111) begin
 		    over_flag <= 1; // if the number of bits is 8, overflow
-		    prob_counter <= 3'b000;
+		    prob_counter <= 7'b0000000;
 	        end
 	        else begin
-	        prob_counter <= prob_counter + 3'b001;
+	        prob_counter <= prob_counter + 7'b0000001;
 	        end
 	    end 
 	    
-		if (clk_counter == 4'b1000) begin // output only when clk_counter has counted 8 cycles. Skip every 9th bit to output.
-		output_prob <= prob_counter;
-	    overflow <= over_flag;
+			if (clk_counter == 8'b10000000) begin // output only when clk_counter has counted 8 cycles. Skip every 9th bit to output.
+				average <={over_flag,prob_counter, 1'b0} >> 4;
 	    over_flag <= 0; //Reset over_flag
-	    prob_counter <= 3'b000; // Reset prob_counter
-	    clk_counter <= 4'b0000; //Reset clock counter
+	    prob_counter <= 7'b0000000; // Reset prob_counter
+	    clk_counter <= 8'b00000000; //Reset clock counter
 	    end
 	    else begin
-	    clk_counter <= clk_counter + 4'b0001;
+	    clk_counter <= clk_counter + 8'b00000001;
 	    end
     end
 end  
   // All output pins must be assigned. If not used, assign to 0.
-	assign uo_out[0] = 0;
-	assign uo_out[3:1] = output_prob[2:0];
-	assign uo_out[4] = overflow;
-	assign uo_out[7:5] = 0;
+	assign uo_out[7:0] = average[7:0];
   assign uio_out = 0;
   assign uio_oe  = 0;
   // List all unused inputs to prevent warnings
